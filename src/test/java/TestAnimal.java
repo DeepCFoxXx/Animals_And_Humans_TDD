76b4ef80b80b028ff9972cfcb6ba23b69d9672e4
DeepCFoxXx <learncode23@gmail.com>
2018-03-31 15:03:31 +0100

    first commit

---
+++ src/test/java/TestAnimal.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestAnimal {
+
+    Animal animal;
+
+    @Before
+    public void before(){
+        animal = new Animal();
+    }
+
+    @Test
+    public void canEat(){
+        assertEquals( "Nom Nom", animal.eat());
+    }
+
+}
