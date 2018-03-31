ee62b8d7152e36490cd2f8a08302d7c8b6663b1a
DeepCFoxXx <learncode23@gmail.com>
2018-03-31 15:27:02 +0100

    human can breathe passing

--- src/test/java/TestHuman.java
+++ src/test/java/TestHuman.java
@@ -11,9 +11,15 @@ public class TestHuman {
     public void before(){
         Kara = new Human();
     }
+
     @Test
     public void canEat(){
         assertEquals("Nom Nom", Kara.eat());
+    }
+
+    @Test
+    public void canBreathe(){
+        assertEquals("In and out", Kara.breathe());
     }
 
 }
